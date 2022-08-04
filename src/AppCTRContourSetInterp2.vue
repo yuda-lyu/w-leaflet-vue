1b5856963d08064b6f0a3f0cfbaf7c41d008c34c
yuda-lyu <firsemisphere@gmail.com>
2022-08-04 23:21:48 +0800

    modify: change interp2 to wk.

--- src/AppCTRContourSetInterp2.vue
+++ src/AppCTRContourSetInterp2.vue
@@ -10,10 +10,14 @@
         <div style="display:flex; padding-bottom:20px;">
 
             <div style="padding:0px 20px;">
+
+                <div>{{msg}}</div>
+
                 <WLeafletVue
                     style="width:800px; height:500px;"
                     :opt="opt"
                 ></WLeafletVue>
+
             </div>
 
             <div style="width:600px; padding:0px 20px 0px 0px; position:relative;">
@@ -44,72 +48,108 @@ export default {
         WLeafletVue,
     },
     data: function() {
-
-        //w-gis
-        let wg = window['w-gis']
-
-        //ptsStation
-        let ptsStation = window.dataRain
-        console.log('ptsStation', ptsStation)
-
-        //ptsClip
-        let ptsClip = window.dataRainClip
-        console.log('ptsClip', ptsClip)
-
-        //ptsContour
-        let ptsContour = []
-        for (let i = 0; i < ptsStation.length; i++) {
-            let v = ptsStation[i]
-            //ptsContour.push([v.latLng[0], v.latLng[1], v.rain])
-            ptsContour.push({
-                x: v.latLng[0],
-                y: v.latLng[1],
-                z: v.rain,
-            })
-        }
-        console.log('ptsContour(ori)', JSON.parse(JSON.stringify(ptsContour)))
-
-        //pis
-        let pis = []
-        //leaflet是緯精度
-        let xmin = 21.83
-        let xmax = 25.30
-        let ymin = 119.95
-        let ymax = 122.04
-        for (let x = xmin; x <= xmax; x += 0.025) {
-            for (let y = ymin; y <= ymax; y += 0.025) {
-                pis.push({ x: x, y: y })
-            }
-        }
-        console.log('pis(matrix)', JSON.parse(JSON.stringify(pis)))
-
-        //剔除位於擴大台灣外框外之點, 但會造成後續計算contour時出錯, 故先取消
-        // let pisTemp = []
-        // for (let i = 0; i < pis.length; i++) {
-        //     let p = [pis[i].x, pis[i].y] //isPointInPolygon須使用[x,y]座標陣列
-        //     let b = wg.isPointInPolygon(p, ptsClip)
-        //     if (b) {
-        //         pisTemp.push(pis[i])
-        //     }
-        // }
-        // pis = pisTemp
-        // console.log('pis(in ptsClip)', JSON.parse(JSON.stringify(pis)))
-
-        //interp2
-        pis = wg.interp2(ptsContour, pis)
-        console.log('pis(interp2)', JSON.parse(JSON.stringify(pis)))
-
-        //產生高密度計算contour用點陣列ptsContour
-        ptsContour = pis.map((v) => {
-            return [v.x, v.y, v.z]
-        })
-        console.log('ptsContour(use)', ptsContour)
-
         return {
             'opt': {
                 center: [24.084, 121.068],
                 zoom: 8,
-                contourSets: [
+                contourSets: [],
+                pointSets: [],
+            },
+            'msg': '',
+            'action': [
+            ],
+        }
+    },
+    mounted: function() {
+        let vo = this
+        // vo.showOptJson()
+        vo.genContours()
+    },
+    watch: {
+        opt: {
+            handler: function() {
+                let vo = this
+                vo.showOptJson()
+            },
+            deep: true,
+        },
+    },
+    methods: {
+        showOptJson: function() {
+            let vo = this
+            jv(vo.opt, document.querySelector('#optjson'), { expanded: true })
+        },
+        genContours: function() {
+            let vo = this
+
+            async function core() {
+
+                //w-gis/interp2
+                let interp2 = window['interp2']
+
+                //ptsStation
+                let ptsStation = window.dataRain
+                console.log('ptsStation', ptsStation)
+
+                //ptsClip
+                let ptsClip = window.dataRainClip
+                console.log('ptsClip', ptsClip)
+
+                //ptsContour
+                let ptsContour = []
+                for (let i = 0; i < ptsStation.length; i++) {
+                    let v = ptsStation[i]
+                    //ptsContour.push([v.latLng[0], v.latLng[1], v.rain])
+                    ptsContour.push({
+                        x: v.latLng[0],
+                        y: v.latLng[1],
+                        z: v.rain,
+                    })
+                }
+                console.log('ptsContour(ori)', JSON.parse(JSON.stringify(ptsContour)))
+
+                //pis
+                let pis = []
+                //leaflet是緯精度
+                let xmin = 21.83
+                let xmax = 25.30
+                let ymin = 119.95
+                let ymax = 122.04
+                for (let x = xmin; x <= xmax; x += 0.025) {
+                    for (let y = ymin; y <= ymax; y += 0.025) {
+                        pis.push({ x: x, y: y })
+                    }
+                }
+                console.log('pis(matrix)', JSON.parse(JSON.stringify(pis)))
+
+                //剔除位於擴大台灣外框外之點, 但會造成後續計算contour時出錯, 故先取消
+                // let pisTemp = []
+                // for (let i = 0; i < pis.length; i++) {
+                //     let p = [pis[i].x, pis[i].y] //isPointInPolygon須使用[x,y]座標陣列
+                //     let b = wg.isPointInPolygon(p, ptsClip)
+                //     if (b) {
+                //         pisTemp.push(pis[i])
+                //     }
+                // }
+                // pis = pisTemp
+                // console.log('pis(in ptsClip)', JSON.parse(JSON.stringify(pis)))
+
+                //interp2
+                let interp2Opt = {
+                    method: 'naturalNeighbor', //naturalNeighbor, kriging
+                    scale: 1000000,
+                }
+                pis = await interp2(ptsContour, pis, interp2Opt)
+                console.log('pis(interp2)', JSON.parse(JSON.stringify(pis)))
+
+                //產生高密度計算contour用點陣列ptsContour
+                ptsContour = pis.map((v) => {
+                    return [v.x, v.y, v.z]
+                })
+                console.log('ptsContour(use)', ptsContour)
+
+                //contourSets
+                let contourSets = [
                     {
                         title: 'Rain contour',
                         msg: 'data from CWB',
@@ -128,8 +168,10 @@ export default {
                         visible: true,
                         order: 0,
                     },
-                ],
-                pointSets: [
+                ]
+
+                //pointSets
+                let pointSets = [
                     {
                         title: 'Rain point',
                         msg: 'data from CWB',
@@ -146,29 +188,30 @@ export default {
                             return c
                         },
                     },
-                ],
-            },
-            'action': [
-            ],
-        }
-    },
-    mounted: function() {
-        let vo = this
-        vo.showOptJson()
-    },
-    watch: {
-        opt: {
-            handler: function() {
-                let vo = this
-                vo.showOptJson()
-            },
-            deep: true,
-        },
-    },
-    methods: {
-        showOptJson: function() {
-            let vo = this
-            jv(vo.opt, document.querySelector('#optjson'), { expanded: true })
+                ]
+
+                return {
+                    contourSets,
+                    pointSets,
+                }
+            }
+
+            //msg
+            vo.msg = 'interp2: calculating contours...'
+
+            //core
+            core()
+                .then((res) => {
+                    vo.opt.contourSets = res.contourSets
+                    vo.opt.pointSets = res.pointSets
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+                .finally(() => {
+                    vo.msg = ''
+                })
+
         },
     },
 }
