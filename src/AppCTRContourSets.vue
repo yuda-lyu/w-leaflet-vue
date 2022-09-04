99caf856eceade69ea25b100e89d6b483638d266
yuda-lyu <firsemisphere@gmail.com>
2022-09-04 10:28:57 +0800

    modify: adj. exam.

--- src/AppCTRContourSets.vue
+++ src/AppCTRContourSets.vue
@@ -44,6 +44,22 @@ export default {
         WLeafletVue,
     },
     data: function() {
+        let pscA = [
+            [24.325, 120.786, 2], [23.944, 120.968, 10], [24.884, 121.234, 20], [24.579, 121.345, 78], [24.664, 121.761, 40], [23.803, 121.397, 30],
+            [23.727, 120.772, 5], [23.539, 120.975, 3], [23.612, 121.434, 8],
+            [23.193, 120.355, 22], [23.456, 120.890, 42], [23.280, 120.551, 25], [23.162, 121.247, 5],
+        ]
+        // pscA = pscA.map((v) => {
+        //     return [v[0], v[1], -v[2]] //相反z值
+        // })
+        let psA = pscA.map((v, k) => {
+            let r = {
+                title: `A-${k + 1}`,
+                msg: `lat:${v[0]}, lng:${v[1]}, value:${v[2]}`,
+                latLng: [v[0], v[1]],
+            }
+            return r
+        })
         return {
             'opt': {
                 center: [24.084, 121.068],
@@ -52,12 +68,7 @@ export default {
                     {
                         title: 'contourSet A',
                         msg: 'msg from contourSet A',
-                        points: [
-                            [24.325, 120.786, 5], [23.944, 120.968, 10], [24.884, 121.234, 20], [24.579, 121.345, 78], [24.664, 121.761, 40], [23.803, 121.397, 30],
-                            [23.727, 120.772, 2], [23.539, 120.975, 3], [23.612, 121.434, 8],
-                            // [24.311, 121.116, 17], [24.351, 121.399, -4],
-                            [23.193, 120.355, 22], [23.456, 120.890, 42], [23.280, 120.551, 25], [23.162, 121.247, 5],
-                        ],
+                        points: pscA,
                         visible: true,
                     },
                     {
@@ -67,6 +78,24 @@ export default {
                             [22.607, 120.416, 0], [22.967, 120.663, 15], [22.592, 120.922, 20], [22.717, 120.644, 45],
                         ],
                         visible: false,
+                    },
+                ],
+                pointSets: [
+                    {
+                        title: 'pointSet A',
+                        msg: 'msg from pointSet A',
+                        points: psA,
+                        visible: false,
+                        popup: function(v) {
+                            console.log('pointSet A popup', v)
+                            let c = `
+                                <div style="padding:15px;">
+                                <div style="color:#222; font-size:0.9rem; white-space:nowrap;"><span style="color:#f26;">[${v.pointSet.title}]</span> ${v.point.title}</div>
+                                <div style="color:#666;">${v.point.msg}</div>
+                                </div>
+                            `
+                            return c
+                        },
                     },
                 ],
             },
