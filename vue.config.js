6472d6566f9a8566e09ab08161dfcebafd1f6877
yuda-lyu <firsemisphere@gmail.com>
2023-10-25 11:15:13 +0800

    modify: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,6 @@ module.exports = {
             },
         }
     },
-    transpileDependencies: ['vuetify'],
-    //publicPath: process.env.NODE_ENV === 'production' ? '/apimgr/' : '/',
+    //transpileDependencies: ['abc'],
+    //publicPath: process.env.NODE_ENV === 'production' ? '/abc/' : '/',
 }
