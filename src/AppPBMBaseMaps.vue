50d5cde3b8cab00052d70ae37460dcc99739de86
yuda-lyu <firsemisphere@gmail.com>
2026-03-02 09:14:58 +0800

    modify: exam.

--- src/AppPBMBaseMaps.vue
+++ src/AppPBMBaseMaps.vue
@@ -44,13 +44,19 @@ export default {
         WLeafletVue,
     },
     data: function() {
+        let tokenMapbox = [
+            'pk',
+            'eyJ1Ijoic2VtaXNwaGVyZSIsImEiOiJja2s1anBrZzMwN3NkMndsOGt6MHo5ajI5In0',
+            '_vUKnQ57n7UcWsWgOPIEgQ',
+        ]
+        tokenMapbox = tokenMapbox.join('.')
         return {
             'opt': {
                 panelBaseMaps: {
                     baseMaps: [
                         {
                             name: 'Mapbox',
-                            url: '//api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2VtaXNwaGVyZSIsImEiOiJja2s1anBrZzMwN3NkMndsOGt6MHo5ajI5In0._vUKnQ57n7UcWsWgOPIEgQ',
+                            url: '//api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=' + tokenMapbox,
                             visible: true,
                         },
                         {
