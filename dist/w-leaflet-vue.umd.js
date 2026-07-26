977371cd0816ca2f50855ab406f00c271e84eb18
yuda-lyu <firsemisphere@gmail.com>
2026-07-26 22:32:38 +0800

    modify: update pks

--- dist/w-leaflet-vue.umd.js
+++ dist/w-leaflet-vue.umd.js
@@ -1,5 +1,5 @@
 /*!
- * w-leaflet-vue v1.0.79
+ * w-leaflet-vue v1.0.80
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- docs/examples/app.umd.js
+++ docs/examples/app.umd.js
@@ -1,5 +1,5 @@
 /*!
- * app v1.0.79
+ * app v1.0.80
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
